e9cd3ae9e0902e79942634b3687b6cbad19ded6f
sajadbasher <104499822+sajadbasher@users.noreply.github.com>
2023-11-16 16:05:59 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -272,5 +272,27 @@ function rotationHandler(rotation) {
 function intervalHandler(interval) {
   document.getElementById("moInterval").innerHTML = interval;
 }
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
+    });
+}
 
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
+
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  
 
