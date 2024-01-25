5f234f184615ff655bd4b27de9e98c03e0737c7a
sajadbasher <104499822+sajadbasher@users.noreply.github.com>
2024-01-25 16:14:30 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,61 +1,15 @@
-const staticDevCoffee = "dev-coffee-site-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
-];
-
-// Installations-Event
-self.addEventListener("install", installEvent => {
-  console.log("Service Worker: Installations-Event wird verarbeitet.");
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      console.log("Service Worker: Caching von Assets wird gestartet.");
-      return cache.addAll(assets);
-    }).then(() => {
-      console.log("Service Worker: Alle Assets wurden erfolgreich gecacht.");
-    })
-  );
-});
-
-// Aktivierungs-Event
-self.addEventListener("activate", event => {
-  console.log("Service Worker: Aktivierungs-Event wird verarbeitet.");
-  // Löschen alter Caches
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticDevCoffee)
-        .map(key => {
-          console.log(`Service Worker: Löschen des alten Cache: ${key}`);
-          return caches.delete(key);
-        })
-      );
-    }).then(() => {
-      console.log("Service Worker: Alte Caches wurden erfolgreich gelöscht.");
-    })
-  );
-});
-
-// Fetch-Event
-self.addEventListener("fetch", fetchEvent => {
-  console.log(`Service Worker: Fetch-Event für: ${fetchEvent.request.url}`);
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request).then(response => {
-        console.log(`Service Worker: Ressource gefetched: ${fetchEvent.request.url}`);
-        return response;
-      });
-    })
-  );
-});
+{
+  "name": "Meine PWA",
+  "short_name": "PWA",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "description": "Eine einfache Progressive Web App",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
